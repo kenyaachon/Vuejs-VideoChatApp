10460f115b1b31a10c9eeb3a3b158821e11934b9
mosesmbugua <swahili1963@gmail.com>
2023-02-26 21:15:21 -0800

    Add Navigation Routing

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { createApp } from "vue"
 import { createRouter, createWebHistory } from "vue-router"
 import App from "./App.vue"
 import Home from "./components/Home.vue"
+import Login from "./components/Login.vue"
 
 import "./assets/main.css"
 
-const routes = [{ path: "/", component: Home }]
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/login", name: "Login", component: Login }
+]
 
 // Create the router instance and pass the `routes` option
 const router = createRouter({
