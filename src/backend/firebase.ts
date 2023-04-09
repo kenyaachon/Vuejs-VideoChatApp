2c7657249d56a7b4d6773eb19bd6334424f07697
mosesmbugua <swahili1963@gmail.com>
2023-04-09 16:07:57 -0700

    Enable getting collections fro firebase

--- src/backend/firebase.ts
+++ src/backend/firebase.ts
@@ -1,6 +1,14 @@
 import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 
 import { firebaseConfig } from "@/secrets"
 
 // base firebase setting
 export const firebaseApp = initializeApp(firebaseConfig)
+
+// Initalize Firebase Authentication
+export const auth = getAuth(firebaseApp)
+
+// Initialize Cloud Firestore
+export const db = getFirestore(firebaseApp)
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,34 @@
 <template>
   <div>
     <h1>Home Page</h1>
+    <p>Welcome {{ user }}</p>
   </div>
 </template>
 
 <script lang="ts">
-export default {}
+import { db } from "../backend/firebase"
+import { collection, getDocs } from "firebase/firestore"
+export default {
+  name: "home",
+  data: function () {
+    return {
+      user: "hello butkis"
+    }
+  },
+  async mounted() {
+    // db.collection("users")
+    //   .doc("2BpRHmgdLtYQHOcwHtqU")
+    //   .get()
+    //   .then(snapshot => {
+    //     this.user = snapshot.data().name
+    //   })
+    const querySnapshot = await getDocs(collection(db, "users"))
+    console.log("the authorization worked")
+    // not the most ideal, but since I only have document in the collection this
+    // should work
+    querySnapshot.forEach(doc => {
+      this.user = doc.data().name
+    })
+  }
+}
 </script>
