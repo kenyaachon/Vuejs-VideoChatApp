f364981c35eafad9929baba0df621c19b244b9e2
mosesmbugua <swahili1963@gmail.com>
2023-04-09 17:40:46 -0700

    Add more styled navigation bar for

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,20 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link>
-  </div>
-  <div class="container text-center">
-    <div class="row justify-content-center">
-      <div class="col-10 col-md-10 col-lg-8 col-xl-7">
-        <h4 class="display-4 text-primary mb-2">Video Chat</h4>
-        <p class="lead">
-          This app uses Firebase for real time communication and WebRTC to create video chats
-          between multiple users, allowing you to create rooms for your meetings and invite
-          attendes.
-        </p>
-        <router-link class="btn btn-outline-primary mr-2" to="/login">Log In</router-link>
+  <div>
+    <nav class="navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">
+          <span class="h2 pr-1">Video Chat</span>
+          <span class="navbar-text small">
+            : Hi <span class="font-weight-bold text-white"> {{ user }}</span>
+          </span>
+        </router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link to="/login" class="nav-item nav-link">log in</router-link>
+        </div>
       </div>
-    </div>
+    </nav>
   </div>
 </template>
 
