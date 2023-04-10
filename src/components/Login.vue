7b3ce90f498694194f757c6328d1703c171e5666
mosesmbugua <swahili1963@gmail.com>
2023-04-09 20:09:31 -0700

    add login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,52 @@
 <template>
-  <div class="login">
+  <!-- <div class="login">
     <h1>Login Page</h1>
+  </div> -->
+  <div>
+    <form class="mt-3" @submit.prevent="login">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <div class="card bg-light">
+              <div class="card-body form-text">
+                <h3 class="font-weight-light mb-3">Log in</h3>
+                <section class="form-group">
+                  <div class="col-12 alert alert-danger px-3" v-if="error">error</div>
+                  <label class="form-control-label sr-only" for="Email">Email</label>
+                  <input
+                    required
+                    class="form-control"
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                  />
+                </section>
+                <section class="form-group">
+                  <input required class="form-control" type="password" placeholder="Password" />
+                </section>
+                <div class="form-group text-right mb-0">
+                  <button class="btn btn-primary" type="submit">Log in</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: "LoginVue"
+  name: "LoginVue",
+  methods: {
+    login(event) {}
+  }
 }
 </script>
+
+<style>
+.form-text {
+  color: black;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,20 @@ const router = createRouter({
   routes: routes
 })
 
+// Vuetify
+
+// import "vuetify/styles"
+// import { createVuetify } from "vuetify"
+// import * as components from "vuetify/components"
+// import * as directives from "vuetify/directives"
+
+// const vuetify = createVuetify({
+//   components,
+//   directives
+// })
+
 const app = createApp(App)
 
 app.use(router)
+// app.use(vuetify)
 app.mount("#app")
